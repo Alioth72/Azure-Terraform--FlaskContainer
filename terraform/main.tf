5dd00910cc77ad5c6c98e93a83d2d6251dc7697c
Aaarat <aaarat72@gmail.com>
2026-06-29 16:31:25 +0530

    ci: add Azure RM remote backend for Terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "sttfstatealioth72"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
